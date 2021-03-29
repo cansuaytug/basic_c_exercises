58ba87f26bb1f2bd763856a4f3754b1ffe9bb3ef
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 19:59:22 +0300

    Add files via upload

---
+++ 1den dışarıya girileb bir sayıya kadar yazdıran.c
@@ -0,0 +1,15 @@
+//1den dýþariya girilen bir sayiya kadar olan program
+
+#include <stdio.h>
+	
+	int main() {
+		int i,sayi;
+		scanf("%d",&sayi);
+		
+		for(i=1;i<=sayi;i++) {
+		
+		
+			printf("%d\n",i);
+	}
+		return 0;
+	}
