6ba6c0baddbd16e118e64388705a47997e5a1cfc
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 19:56:00 +0300

    Add files via upload

---
+++ 1den 10a kadar alt alta.c
@@ -0,0 +1,14 @@
+//1den 10a kadar olan sayýlarý (10 dahil) alt alta yazan c programý
+
+#include <stdio.h>
+	int main () {
+		int sayac;
+		sayac=1;
+		
+		while(sayac<=10) 
+			{
+				printf("%d\n",sayac);
+				sayac++;
+			}
+		return 0;
+	}
