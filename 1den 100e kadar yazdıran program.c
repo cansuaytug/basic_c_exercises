ceb4a95afba7f0e626f40834d3eb744d98b9ad62
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 19:56:54 +0300

    Add files via upload

---
+++ 1den 100e kadar yazdıran program.c
@@ -0,0 +1,14 @@
+//1den 100e kadar olan sayýlarý(100 hariç) ekrana yazan program
+
+
+#include <stdio.h>
+
+	int main() {
+		int i;
+		
+		for(i=1;i<100;i++)	{
+			
+			printf("%d\n",i);
+		}	
+		return 0;
+	}
