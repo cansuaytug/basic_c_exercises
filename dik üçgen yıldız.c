7a03118d8c45cc150ddb1481dffd71ef610b628f
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 20:18:39 +0300

    Add files via upload

---
+++ dik üçgen yıldız.c
@@ -0,0 +1,24 @@
+// yýldýzdan dik üçgen yap
+#include <stdio.h>
+	int main () {
+		int i,j;
+		
+		for(i=1;i<=25;i++) {
+			
+			
+			for(j=1;j<=i;j++)
+			{
+				printf("*");
+			}
+			
+			printf("\n");
+			
+		}
+		
+		
+		
+			
+			return 0; 	
+		
+		
+		}
