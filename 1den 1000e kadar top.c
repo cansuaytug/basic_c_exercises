96c09010d101f68eb2f3f2b1ff87c1dd8254697e
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 19:57:20 +0300

    Add files via upload

---
+++ 1den 1000e kadar top.c
@@ -0,0 +1,18 @@
+//1'den 1000'e kadar olan sayýlarýn toplamýný Toplam formülü kullanmadan bulan C programýný yazýnýz. 
+
+
+#include <stdio.h>
+
+	int main(){
+		int i,top;
+		top=0;
+		for(i=1;i<=1000;i++)
+		
+	 	top=top+i;
+		
+		printf("%d",top);
+		
+		
+		return 0;
+		
+		}
