4ce4672ce449303328f6f6a941c2d9c05f8e480a
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 20:01:10 +0300

    2den başlayarak 100e kadar olan çift sayıları herhangi bir if koşulu kullanmadan ekrana yazan c programı

---
+++ 2den 100e.c
@@ -0,0 +1,12 @@
+//2den baþlayarak 100e kadar olan çift sayýlarý herhangi bir if koþulu kullanmadan ekrana yazan c programý
+
+#include <stdio.h>
+	int main() {
+		int i;
+		
+		for(i=2;i<=100;i=i+2) 
+			printf("%d\n",i);
+			
+			
+		return 0;
+	}
