c9a8c52f3d105f717048c946d92ab9de442354f6
Cansu Aytuğ <65678907+cansuaytug@users.noreply.github.com>
2021-03-29 20:29:56 +0300

    Add files via upload

---
+++ ucgen alanı.c
@@ -0,0 +1,21 @@
+//ücgen alaný hesaplama
+
+#include <stdio.h>
+
+	int main () {
+		
+		
+		int taban,yuks,alan;
+		
+		scanf("%d %d",&taban,&yuks);
+		
+		alan=taban*yuks/2;
+		
+		printf("%d",alan);
+		
+		
+		return 0;
+		
+		
+		
+	}
